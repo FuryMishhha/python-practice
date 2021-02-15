3998b9860cec150eaedf77af051d6c77342ce847
Misha <57438792+FuryMishhha@users.noreply.github.com>
2021-02-15 15:33:10 +0300

    Delete Pr1 directory

--- Pr1/main.py
+++
@@ -1,7 +0,0 @@
-from p1_1 import zad1_1
-print()
-from p1_2 import zad1_2
-print()
-from p1_3 import zad1_3
-print()
-from p1_4 import zad1_4
--- Pr1/p1_1.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def zad1_1(x,y):
-    return (79*x**3-24*y**6)/(y**5+abs(y))+(2*x**8-y**7)/(2*x**7+x**8)-(y**3+19*x**5)/(32*y**3-math.sin(x))
-
-print(f'{zad1_1(-39, 17):.2e}', f'{zad1_1(-88, 81):.2e}', sep='\n')
